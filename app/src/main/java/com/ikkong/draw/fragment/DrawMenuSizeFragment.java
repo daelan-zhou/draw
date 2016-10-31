1e11e97a014730d17637dcd5e6c586d90a8ba3fb
ikkong.maipu <ikkong@qq.com>
2016-10-31 16:29:40 +0800

    size fragment

--- app/src/main/java/com/ikkong/draw/fragment/DrawMenuSizeFragment.java
+++ app/src/main/java/com/ikkong/draw/fragment/DrawMenuSizeFragment.java
@@ -3,6 +3,7 @@ package com.ikkong.draw.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,9 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.ikkong.draw.R;
+import com.ikkong.draw.popupwindow.SeekBarPopWindow;
+import com.labo.kaji.relativepopupwindow.RelativePopupWindow.HorizontalPosition;
+import com.labo.kaji.relativepopupwindow.RelativePopupWindow.VerticalPosition;
 
 /**
  * Author:  ikkong
@@ -18,14 +22,15 @@ import com.ikkong.draw.R;
  * Description:
  */
 
-public class DrawMenuSizeFragment extends Fragment implements View.OnClickListener{
+public class DrawMenuSizeFragment extends Fragment implements View.OnClickListener {
     LinearLayout laySize;
     TextView tvSize;
+    int size;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = View.inflate(getActivity(), R.layout.draw_menu_size,null);
+        View v = View.inflate(getActivity(), R.layout.draw_menu_size, null);
         laySize = (LinearLayout) v.findViewById(R.id.lay_size);
         tvSize = (TextView) v.findViewById(R.id.tv_size);
         laySize.setOnClickListener(this);
@@ -34,8 +39,22 @@ public class DrawMenuSizeFragment extends Fragment implements View.OnClickListen
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.lay_size){
-            //弹出一个seekbar
+        Log.d("DrawMenuSizeFragment", "====lay_size====");
+        
+        //弹出一个seekbar
+        new SeekBarPopWindow(getActivity(),size,listerner).showOnAnchor(laySize,
+                VerticalPosition.BELOW, HorizontalPosition.CENTER);
+    }
+
+    SeekBarPopWindow.onSeekBarValueChangeListerner listerner = new SeekBarPopWindow.onSeekBarValueChangeListerner() {
+        @Override
+        public void onSeekBarValueChange(int i) {
+            size = i;
+            tvSize.setText(size+"");
         }
+    };
+    
+    public int getSize(){
+        return size;
     }
 }
--- app/src/main/java/com/ikkong/draw/popupwindow/SeekBarPopWindow.java
+++ app/src/main/java/com/ikkong/draw/popupwindow/SeekBarPopWindow.java
@@ -1,5 +1,19 @@
 package com.ikkong.draw.popupwindow;
 
+import android.animation.Animator;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.ViewGroup;
+import android.widget.SeekBar;
+
+import com.ikkong.draw.R;
 import com.labo.kaji.relativepopupwindow.RelativePopupWindow;
 
 /**
@@ -10,4 +24,74 @@ import com.labo.kaji.relativepopupwindow.RelativePopupWindow;
  */
 
 public class SeekBarPopWindow extends RelativePopupWindow {
+
+    public SeekBarPopWindow(Context context, int defValue, final onSeekBarValueChangeListerner listerner) {
+        View v = LayoutInflater.from(context).inflate(R.layout.popup_seekbar, null);
+        setContentView(v);
+        SeekBar seekBar = (SeekBar) v.findViewById(R.id.seekbar);
+        seekBar.setProgress(defValue);
+        setWidth(ViewGroup.LayoutParams.WRAP_CONTENT);
+        setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+        setFocusable(true);
+        setOutsideTouchable(true);
+        setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        // Disable default animation for circular reveal
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setAnimationStyle(0);
+        }
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (listerner != null) {
+                    listerner.onSeekBarValueChange(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    @Override
+    public void showOnAnchor(@NonNull View anchor, int vertPos, int horizPos, int x, int y) {
+        super.showOnAnchor(anchor, vertPos, horizPos, x, y);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            circularReveal(anchor);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void circularReveal(@NonNull final View anchor) {
+        final View contentView = getContentView();
+        contentView.post(new Runnable() {
+            @Override
+            public void run() {
+                final int[] myLocation = new int[2];
+                final int[] anchorLocation = new int[2];
+                contentView.getLocationOnScreen(myLocation);
+                anchor.getLocationOnScreen(anchorLocation);
+                final int cx = anchorLocation[0] - myLocation[0] + anchor.getWidth() / 2;
+                final int cy = anchorLocation[1] - myLocation[1] + anchor.getHeight() / 2;
+
+                contentView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+                final int dx = Math.max(cx, contentView.getMeasuredWidth() - cx);
+                final int dy = Math.max(cy, contentView.getMeasuredHeight() - cy);
+                final float finalRadius = (float) Math.hypot(dx, dy);
+                Animator animator = ViewAnimationUtils.createCircularReveal(contentView, cx, cy, 0f, finalRadius);
+                animator.setDuration(300);
+                animator.start();
+            }
+        });
+    }
+
+    public interface onSeekBarValueChangeListerner {
+        void onSeekBarValueChange(int i);
+    }
 }
